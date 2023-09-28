dba1e725234c4669a4ed9af0fb9cdf9c82f239ed
haggsss <145889743+haggsss@users.noreply.github.com>
2023-09-28 22:21:53 +0700

    algo.py

---
+++ algo.py
@@ -0,0 +1,7 @@
+nama = input("nama kamu siapa? ") 
+asal = input("tempat tinggal? " )
+sekolah = input("asal sekolah dari mana? ") 
+print("hallo selamat malam " +nama) 
+print("ohh kamu dari " +asal)
+print("ohh kamu dari sekolah " +sekolah)
+
